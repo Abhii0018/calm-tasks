51253aafe0035cb86c7538589e9620b86bcbabcd
Abhishek Kumar <abhisheksah018@gmail.com>
2025-12-30 03:45:37 +0530

    fix :use env based API url

--- calm-tasks/src/services/api.js
+++ calm-tasks/src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
  * Base Axios instance
  */
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 /**
