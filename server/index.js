246e32d039d012bd390712393a3ed35161a619a4
Abhishek Kumar <abhisheksah018@gmail.com>
2025-12-30 03:28:50 +0530

    fix: use Render dynamic PORT

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ mongoose
   .catch((err) => console.log("Mongo error:", err));
 
 /* ================= SERVER ================= */
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
